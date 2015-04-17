5545484b7fdad7b8ee9c2f9be4f52b69dcfa739c
jimmy <jimmy.huangjin@gmail.com>
2015-04-17 16:07:40 +0800

    add another layout

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,3 +1,14 @@
 class JokesController < ApplicationController
-  
+  def index
+  end
+
+  def show
+    render_layout
+  end
+
+  private
+
+  def render_layout
+    render :layout => 'detail'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'jokes#index'
+  resources :jokes
 end
