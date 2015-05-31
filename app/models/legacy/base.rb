6346b09fa3f104be10ffd11be89bcb5eb7f2ef74
jimmy <jimmy.huangjin@gmail.com>
2015-05-31 10:49:27 +0800

    add application

--- app/models/legacy/base.rb
+++ app/models/legacy/base.rb
@@ -1,9 +1,16 @@
 class Legacy::Base < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection({
+
+  conf = {
     adapter: 'postgresql',
     encoding: 'unicode',
     pool: 5,
     database: 'bx_legacy'
-  })
+  }
+
+  if Rails.env.production?
+    conf.merge!(user: 'kaixin', password: '_kx_1324')
+  end
+
+  establish_connection conf
 end
