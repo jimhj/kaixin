1960a5b0b41a7da0f8e7c800226bad31e2010c84
jimmy <jimmy.huangjin@gmail.com>
2015-06-08 21:42:55 +0800

    add baidu & alimama

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,13 +2,20 @@ class Ad < ActiveRecord::Base
   validates_presence_of :name, :ad_type, :body
 
   TYPE = {
-    "右侧顶部广告位"            => "RIGHT_SIDE_TOP",
-    "右侧跟随广告位"            => "RIGHT_SIDE_FIXED",
-    "内页正文下方广告位"       => "DETAIL",
-    "内页推荐图片下方广告位"    => "RECOMMEND"
+    "右侧顶部广告位" => "RIGHT_SIDE_TOP",
+    "右侧跟随广告位" => "RIGHT_SIDE_FIXED",
+    "内页正文下方广告位" => "DETAIL",
+    "内页推荐图片下方广告位" => "RECOMMEND"
   }
 
   scope :published, -> { where(published: true) }
   
-  scope :right_site_top, -> { published.where(ad_type: 'RIGHT_SIDE_TOP') }
+  class << self
+    Ad::TYPE.values.each do |ad_type|
+      define_method(:"#{ad_type.downcase}") do
+        published.where(ad_type: ad_type)
+      end
+    end
+  end
+
 end
