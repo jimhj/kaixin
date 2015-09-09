c2d21afeb21b2865e871837e3c39b70a9f086350
jimmy <jimmy.huangjin@gmail.com>
2015-09-09 21:19:43 +0800

    fixbug

--- app/controllers/ad_controller.rb
+++ app/controllers/ad_controller.rb
@@ -21,9 +21,10 @@ class AdController < ApplicationController
 
   def pics
     @number = params[:number].presence || 8
+    @number = @number.to_i
     @gap = params[:gap]
     @hmsr = params[:c]
-    @recommends = Joke.recommend(number.to_i)
+    @recommends = Joke.recommend(@number)
     headers['X-Frame-Options'] = "ALLOWALL"
     render template: 'ad/pics', layout: false
   end
